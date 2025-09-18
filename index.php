3b08d7238908de7cfaeb456f772903591e0ff2a7
Mateus Yan <49455758+GWG29@users.noreply.github.com>
2025-09-18 00:22:29 +0000

    Criado código do arquivo Pessoa

--- index.php
+++ index.php
@@ -1,16 +1,17 @@
 <?php
 
 require_once __DIR__ . '/src/Core/Autoloader.php';
+require_once __DIR__ . '/src/Models/Pessoa.php';
 
-use Models\Cliente;
 use Models\Pessoa;
 
 // Instanciando normalmente, sem 'use'
 $cliente = new Pessoa();
 
 $cliente->nome=readline("Digite o nome do cliente: ");
+$cliente->sobrenome=readline("Digite o sobrenome do cliente: ");
 $cliente->idade=readline("Digite a idade do cliente: ");
-$cliente->cpf=readline("Digite o CPF do cliente: ");
-$cliente->rg=readline("Digite o RG do cliente: ");
-$cliente->endereco=readline("Digite o endereço do cliente: ");
-echo "Nome: $cliente->nome\nIdade: $cliente->idade\nCPF: $cliente->cpf\nRG: $cliente->rg\nEndereço: $cliente->endereco\n";
+$cliente->setCpf(readline("Digite o CPF do cliente: "));
+$cliente->setRg(readline("Digite o RG do cliente: "));
+$cliente->setEndereco(readline("Digite o endereço do cliente: "));
+echo "Nome: " . $cliente->getNomeCompleto() . "\nIdade: " . $cliente->idade . "\nCPF: " . $cliente->getCpf() . "\nRG: " . $cliente->getRg() . "\nEndereço: " . $cliente->getEndereco() . "\n";
--- src/Models/Pessoa.php
+++ src/Models/Pessoa.php
@@ -1,18 +1,43 @@
 <?php 
 namespace Models;
 
+
 class Pessoa {
     public string $nome;
+    public string $sobrenome;
     public int $idade;
-    public string $cpf;
-    public string $rg;
-    public string $endereco;
+    private string $cpf;
+    private string $rg;
+    private string $endereco;
 
-    public function __construct($nome, $idade, $cpf, $rg, $endereco) {
+    public function setEndereco($endereco) {
+        $this->endereco = $endereco;
+    }
+
+    public function __construct($nome = "", $sobrenome = "", $idade = 0, $cpf = "", $rg = "", $endereco = "") {
         $this->nome = $nome;
+        $this->sobrenome = $sobrenome;
         $this->idade = $idade;
         $this->cpf = $cpf;
         $this->rg = $rg;
         $this->endereco = $endereco;
+    }
+    public function getCpf() {
+        return $this->cpf;
+    }
+    public function getRg() {
+        return $this->rg;
+    }
+    public function getEndereco() {
+        return $this->endereco;
+    }
+    public function setCpf($cpf) {
+        $this->cpf = $cpf;
+    }
+    public function setRg($rg) {
+        $this->rg = $rg;
+    }
+    public function getNomeCompleto(){
+        return $this->nome . " " . $this->sobrenome;
     }
 }
