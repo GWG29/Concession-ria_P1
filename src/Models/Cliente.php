a98242a954cdcb209197e30b34f556f25d340224
DAloiaMarcos <106317871+DAloiaMarcos@users.noreply.github.com>
2025-09-18 00:38:13 +0000

    Base classe Cliente

--- src/Models/Cliente.php
+++ src/Models/Cliente.php
@@ -1,8 +1,24 @@
 <?php
 
 namespace Models;
-use Models\Pessoa;
 
-class Cliente extends Pessoa {
+use src\Models\Pessoa;
+
+class Clientes extends Pessoa{
+    public string $modeloComprado;
+    public string $corCarro;
+    public string $modoPagamento;
+    public bool $querSeguro;
+
+    public function __construct($modeloComprado, $corCarro,$modoPagamento,$querSeguro){
+        parent::__construct($nome,$idade,$cpf,$rg,$endereco);
+        $this->modeloComprado = $modeloComprado;
+        $this->corCarro = $corCarro;
+        $this->modoPagamento = $modoPagamento;
+        // defini como true naturalmente, porém a pessoa vai poder alterar esse valor na compra
+        // entretanto acho que é válido considerar como positivo o fato de querer seguro.
+        $this->querSeguro = true;
+    }
+
 
 }
