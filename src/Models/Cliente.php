999d08103039fcec4bb5983760833d68c1eb4254
Mateus Yan <49455758+GWG29@users.noreply.github.com>
2025-09-18 01:20:43 +0000

    Adicionado opção de decidir se quer seguro ou não

--- src/Models/Cliente.php
+++ src/Models/Cliente.php
@@ -8,16 +8,18 @@ class Clientes extends Pessoa{
     public string $modeloComprado;
     public string $corCarro;
     public string $modoPagamento;
+    public string $Seguro;
     public bool $querSeguro;
 
-    public function __construct($modeloComprado, $corCarro,$modoPagamento,$querSeguro){
+    public function __construct($modeloComprado, $corCarro,$modoPagamento,$Seguro){
         parent::__construct($nome,$idade,$cpf,$rg,$endereco);
         $this->modeloComprado = $modeloComprado;
         $this->corCarro = $corCarro;
         $this->modoPagamento = $modoPagamento;
+        $this->Seguro = $Seguro;
+        strtolower($this->Seguro) === 'sim' ? $this->querSeguro = true : $this->querSeguro = false;
         // defini como true naturalmente, porém a pessoa vai poder alterar esse valor na compra
         // entretanto acho que é válido considerar como positivo o fato de querer seguro.
-        $this->querSeguro = true;
 
         
     }
